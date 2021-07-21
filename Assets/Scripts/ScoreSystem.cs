1744cb0b0102eba2787d50aed041137cfc96a178
MarcoRamos99 <marcoramosg928@gmail.com>
2021-07-21 12:52:36 -0500

    Added high score system

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -8,6 +8,8 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMulitplier;
 
+    public const string HighScoreKey = "HighScore";
+
     private float score;
 
     // Update is called once per frame
@@ -16,5 +18,14 @@ public class ScoreSystem : MonoBehaviour
         score += Time.deltaTime * scoreMulitplier;
         int displayScore = Mathf.FloorToInt(score);
         scoreText.text = displayScore.ToString();
+    }
+
+    private void OnDestroy()
+    {
+       int currentHigshScore =  PlayerPrefs.GetInt(HighScoreKey, 0);
+       if(score> currentHigshScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
